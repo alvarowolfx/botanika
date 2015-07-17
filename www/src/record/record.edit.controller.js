22466321479a8127d8c91fd1aa516408e47655fa
Alvaro Viebrantz <alvarowolfx@gmail.com>
2015-07-16 23:54:17 -0400

    Add photo registration on record and filtering records by research

--- www/src/record/record.edit.controller.js
+++ www/src/record/record.edit.controller.js
@@ -3,14 +3,15 @@
         .module('botanika.research')
         .controller('RecordEditController', RecordEditController);
 
-    RecordEditController.$inject = ['$state', '$ionicHistory', 'Record', '$cordovaGeolocation'];
+    RecordEditController.$inject = ['$state', '$ionicHistory', 'Record', '$cordovaGeolocation', '$cordovaCamera'];
 
-    function RecordEditController($state, $ionicHistory, Record, $cordovaGeolocation) {
+    function RecordEditController($state, $ionicHistory, Record, $cordovaGeolocation, $cordovaCamera) {
         var vm = this;
 
         var researchId = $state.params.researchId;
         var id = $state.params.id;
         vm.isShowing = !!id;
+        vm.cameraAvailable = !!navigator.camera;
 
         vm.saveRecord = saveRecord;
         vm.selectPicture = selectPicture;
@@ -42,29 +43,58 @@
             vm.locationStatus = null;
         }
 
-        function getLocation(){
+        function getLocation() {
             $cordovaGeolocation.getCurrentPosition({
                 enableHighAccuracy: true
-            }).then(function(position){
+            }).then(function(position) {
                 vm.record.location.lat = position.coords.latitude;
                 vm.record.location.lon = position.coords.longitude;
                 vm.locationStatus = 'success';
-            }, function(err){
+            }, function(err) {
                 //Handle geoloc error
                 vm.locationStatus = 'error';
             });
         }
 
         function saveRecord() {
-            $ionicHistory.goBack();
+            var record = angular.copy(vm.record);
+            record.researchId = researchId;
+            Record.save(record).then(function() {
+                $ionicHistory.goBack();
+            }).catch(function(err) {
+                //Handle error
+                console.log(err);
+            });
         }
 
-        function takePicture(){
-
+        function getCommonCameraOption() {
+            return {
+                quality: 50,
+                destinationType: Camera.DestinationType.DATA_URL,
+                allowEdit: true,
+                encodingType: Camera.EncodingType.JPEG,
+                saveToPhotoAlbum: false
+            }
         }
 
-        function selectPicture(){
+        function takePicture() {
+            var options = getCommonCameraOption();
+            options.sourceType = Camera.PictureSourceType.CAMERA;
+            getPictureWithOptions(options);
+        }
 
+        function selectPicture() {
+            var options = getCommonCameraOption();
+            options.sourceType = Camera.PictureSourceType.PHOTOLIBRARY;
+            getPictureWithOptions(options);
+        }
+
+        function getPictureWithOptions(options) {
+            $cordovaCamera.getPicture(options).then(function(imageData) {
+                vm.record.photo = imageData;
+            }, function(err) {
+                // error
+            });
         }
     }
 })();
--- www/src/shared/services/record.js
+++ www/src/shared/services/record.js
@@ -18,15 +18,48 @@
             }
         });
         db.put(recordDesignDoc);
+        var recordByResearchDesignDoc = Database.createDesignDoc('record_by_research', function(doc) {
+            if (doc.type === 'record') {
+                emit(doc.researchId, doc.createdAt);
+            }
+        });
+        db.put(recordByResearchDesignDoc);
 
         return service;
 
-        function allByResearch() {
-            return $q.when([]);
+        function allByResearch(researchId) {
+            return $q.when(db.query('record_by_research', {
+                startkey: researchId,
+                endkey: researchId,
+                descending: true,
+                include_docs: true
+            })).then(function(docs) {
+                return docs.rows;
+            });
+        }
+
+        function load(id){
+            return $q.when(db.get(id));
         }
 
         function save(record) {
-            research.type = 'record';
+            if(record._id){
+                return $q.reject('Record cannot be updated');
+            }
+
+            if(!record.researchId){
+                return $q.reject('Research id is required');
+            }
+            if (record.photo) {
+                record._attachments = {
+                    photo: {
+                        content_type: 'image/png',
+                        data: record.photo
+                    }
+                }
+            }
+            record.type = 'record';
+            record.createdAt = new Date();
             return $q.when(db.post(record));
         }
 
