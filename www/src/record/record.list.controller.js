5017ce61c3d3a8f6a2f6aa6d658fd6e025a16581
Alvaro Viebrantz <alvarowolfx@gmail.com>
2015-07-17 14:13:55 -0400

    Add remove record feature

--- www/src/record/record.list.controller.js
+++ www/src/record/record.list.controller.js
@@ -9,24 +9,33 @@
         var vm = this;
 
         var researchId = $state.params.researchId;
-        $scope.$on('$ionicView.afterEnter',function(){
-            Research.load(researchId).then(function(research){
+        $scope.$on('$ionicView.afterEnter', function() {
+            Research.load(researchId).then(function(research) {
                 $ionicNavBarDelegate.title(research.name);
             });
         });
 
-        $scope.$on('$ionicView.beforeEnter', function(){
+        $scope.$on('$ionicView.beforeEnter', function() {
             vm.records = [];
             reload();
         });
         vm.records = [];
         reload();
 
-        function reload(){
+        vm.remove = remove;
+
+        function reload() {
             vm.loading = true;
             Record.allByResearch(researchId).then(function(records) {
                 vm.loading = false;
                 vm.records = records;
+            });
+        };
+
+        function remove(id) {
+            Record.remove(id).then(function() {
+                vm.records = [];
+                reload();
             });
         };
 
